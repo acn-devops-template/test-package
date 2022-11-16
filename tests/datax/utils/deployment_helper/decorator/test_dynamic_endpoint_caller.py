3395a9ec9ec7c45bbdd60b0faea939171b62cabc
NathNx <natt.research1@gmail.com>
2022-11-16 11:07:24 +0700

    test: add test for dynamic endpoint

---
+++ tests/datax/utils/deployment_helper/decorator/test_dynamic_endpoint_caller.py
@@ -0,0 +1,14 @@
+import subprocess 
+
+
+
+def test_dynamic_endpoint_caller_with_default_arg():
+    
+    #output = subprocess.run(['$python test_dynamic_endpoint.py'])
+    output = exec(open("test_dynamic_endpoint.py -m test_dynamic_endpoint_main").read())
+    
+    print(output)
+    
+    assert output == True
+
+
