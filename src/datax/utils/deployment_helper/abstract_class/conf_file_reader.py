0df58735f6b330b3abd720908e5241ad82ed041f
Nonthapatr <n.sanitprachakorn@accenture.com>
2023-05-24 14:44:45 +0700

    style: :lipstick: applied changes suggested by @byte-supakrid

--- src/datax/utils/deployment_helper/abstract_class/conf_file_reader.py
+++ src/datax/utils/deployment_helper/abstract_class/conf_file_reader.py
@@ -64,7 +64,7 @@ class ConfFileReader(ABC):
         pass
 
     @abstractmethod
-    def read_content(self, content: str, mapping: Dict = {}) -> Union[str, Dict]:
+    def read_content(self) -> Union[str, Dict]:
         """Base method for reading config contents.
 
         Args:
@@ -104,8 +104,8 @@ class YAMLReader(ConfFileReader):
 
         return yaml_dict
 
-    def read_content(self, content: str, mapping: Dict = {}) -> Union[str, Dict]:
-        """Base method for reading config contents.
+    def read_content(self, content: str) -> Union[str, Dict]:
+        """Method for reading YAML configuration string.
 
         Args:
             content (str): content of configuration
@@ -145,8 +145,8 @@ class JSONReader(ConfFileReader):
 
         return json_dict
 
-    def read_content(self, content: str, mapping: Dict = {}) -> Union[str, Dict]:
-        """Base method for reading config contents.
+    def read_content(self, content: str) -> Union[str, Dict]:
+        """Method for reading JSON configuration string.
 
         Args:
             content (str): content of configuration
@@ -177,21 +177,24 @@ class J2Reader(ConfFileReader):
         j2_dict = {}
         for each_file in conf_files:
             file_name_components = os.path.basename(each_file).split(".")
+            file_name = file_name_components[0]
+            original_file_format = file_name_components[1]
+            
             content = pathlib.Path(each_file).read_text()
             conf_txt = self.read_content(content=content, mapping=dict(os.environ))
             conf_txt = str(conf_txt)
 
-            if file_name_components[1] in ["yml", "yaml"]:
+            if original_file_format in ["yml", "yaml"]:
                 config = YAMLReader([]).read_content(content=conf_txt)
-                j2_dict[file_name_components[0]] = config
-            elif file_name_components[1] in ["json"]:
+                j2_dict[file_name] = config
+            elif original_file_format in ["json"]:
                 config = JSONReader([]).read_content(content=conf_txt)
-                j2_dict[file_name_components[0]] = config
+                j2_dict[file_name] = config
 
         return j2_dict
 
-    def read_content(self, content: str, mapping: Dict = {}) -> Union[str, Dict]:
-        """Base method for reading config contents.
+    def read_content(self, content: str, mapping: Dict) -> Union[str, Dict]:
+        """Method for reading J2 configuration template and rendering the template.
 
         Args:
             content (str): content of configuration
