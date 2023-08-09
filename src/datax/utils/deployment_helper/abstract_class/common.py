370e8d8d461133875f8e60037b062ac4853a9934
pchanovan <p.chanovan@accenture.com>
2023-08-09 16:53:43 +0700

    refactor: :construction: add activate_sensor, conf_sensor to be set in base class

--- src/datax/utils/deployment_helper/abstract_class/common.py
+++ src/datax/utils/deployment_helper/abstract_class/common.py
@@ -58,6 +58,7 @@ class Task(ABC):
         * self.conf_spark is a spark configuration if provided.
         * self.conf_logger is a logger configuration if provided.
         * self.conf_audit is a audit configuration if provided.
+        * self.conf_sensor is a sensor configuration.
 
     """
 
@@ -70,10 +71,11 @@ class Task(ABC):
         init_conf_spark: Optional[Dict] = None,
         init_conf_logger: Optional[Dict] = None,
         init_conf_audit: Optional[Dict] = None,
+        activate_sensor: Optional[bool] = False,
     ) -> None:
         """__init__ function of Task class.
 
-        Set following useful objects: self.spark, self.logger, self.dbutils, self.conf_app, self.conf_spark, self.conf_logger, self.conf_audit.
+        Set following useful objects: self.spark, self.logger, self.dbutils, self.conf_app, self.conf_spark, self.conf_logger, self.conf_audit, self.conf_sensor.
 
         Args:
             module_name (str): Use this input as self.module_name.
@@ -83,6 +85,7 @@ class Task(ABC):
             init_conf_spark (Optional[Dict]): If provided, use this input as self.conf_all["spark"], otherwise None.
             init_conf_logger (Optional[Dict]): If provided, use this input as self.conf_all["logger"], otherwise None.
             init_conf_audit (Optional[Dict]): If provided, use this input as self.conf_all["audit"], otherwise None.
+            activate_sensor (Optional[bool]): If provided, use this boolean input to activate sensor, otherwise False.
 
         """
         self.module_name = module_name
@@ -94,6 +97,10 @@ class Task(ABC):
         conf_all["spark"] = init_conf_spark or conf_all.get("spark", {})
         conf_all["logger"] = init_conf_logger or conf_all.get("logger", {})
         conf_all["audit"] = init_conf_audit or conf_all.get("audit", {})
+        conf_all["sensor"] = {
+            "activate_sensor": activate_sensor,
+            "sensor": conf_all["app"].get(module_name).get("sensor"),
+        }
 
         # Set conf to attributes
         self.conf_all = conf_all
