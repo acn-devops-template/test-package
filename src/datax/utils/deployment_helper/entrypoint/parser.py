1cbee95910c9261cb928014eac9dc79d27d9397a
Tanyaaa M <tanyaaluk.munkongpat@accenture.com>
2023-08-10 16:39:27 +0700

    refactor: update flag to activate_job_status_log

--- src/datax/utils/deployment_helper/entrypoint/parser.py
+++ src/datax/utils/deployment_helper/entrypoint/parser.py
@@ -7,11 +7,8 @@ from tap import Tap
 class JobStatusArguments(Tap):
     """Command-line arguments related to job status."""
 
-    activate_etl_sts_log: bool = False
-    """Activate log process to write run result into tbl_etl_sts_log."""
-
-    activate_feat_ml_sts_log: bool = False
-    """Activate log process to write run result into tbl_fp_sts_log."""
+    activate_job_status_log: bool = False
+    """Activate log process to write job execution details into job log table."""
 
     parent_id: str = None
     """Databricks workflow ID."""
@@ -26,4 +23,4 @@ class JobStatusArguments(Tap):
     """Workflow job name."""
 
     task_name: str = None
-    """Job task name"""
+    """Task name within a workflow."""
