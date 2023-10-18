f14cc93a2f669c2d4b4d993997e53aad77aefadf
ahongtrakulchai <129280158+ahongtrakulchai@users.noreply.github.com>
2023-10-18 11:53:04 +0700

    Update __init__.py

--- src/datax/utils/deployment_helper/__init__.py
+++ src/datax/utils/deployment_helper/__init__.py
@@ -4,5 +4,5 @@ __version__ = "0.1.1"  # will be overwritten by poetry_bumpversion
 from django.db import connection
 
 def show_user(request, username):
-  with connection.cursor() as cursor:
-    cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM users WHERE username = '%s'" % username)
